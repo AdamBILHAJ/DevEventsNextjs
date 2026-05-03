ea50b4001c07f0e1da3343c4b8ae1e319f503dcd
adama404 <belhaadam17@gmail.com>
2026-05-03 22:29:50 +0100

    home page 1

--- init-push.bat
+++ init-push.bat
@@ -1,6 +1,19 @@
 @echo off
+REM Ensure we're on master/main first
+git checkout master 2>nul || git checkout main
+
+REM Create develop branch if it doesn't exist
+git show-ref --verify --quiet refs/heads/develop || git branch develop
+
+REM Add files and commit
 git add .
-git branch -c develop
 git commit -m "home page 1"
-git remote add origin https://github.com/AdamBILHAJ/DevEventsNextjs.git
-git push -u origin main
+
+REM Add remote only if it doesn't exist
+git remote get-url origin >nul 2>&1
+if errorlevel 1 (
+    git remote add origin https://github.com/AdamBILHAJ/DevEventsNextjs.git
+)
+
+REM Push current branch to main on remote
+git push -u origin HEAD:main
