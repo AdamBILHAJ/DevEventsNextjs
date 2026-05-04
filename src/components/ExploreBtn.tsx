69736f65ec37d7ce7f1c4eab07d184dbdca18208
adama404 <belhaadam17@gmail.com>
2026-05-04 19:04:59 +0100

    implement db models

--- src/components/ExploreBtn.tsx
+++ src/components/ExploreBtn.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from 'next/image';
+import posthog from 'posthog-js';
 
 function ExploreBtn() {
   return (
@@ -8,7 +9,10 @@ function ExploreBtn() {
         href="#events"
         id="explore-btn"
         className="inline-flex items-center gap-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700"
-        onClick={() => console.log('CLICK')}
+        onClick={() => {
+          console.log('CLICK');
+          posthog.capture('explore_events_clicked');
+        }}
       >
         Explore Events
         <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
